7208d7b3e9e086214868cfb5ca32836f3dbd691b
PES2UG22CS169 <dshreya9686552636@gmail.com>
2025-03-14 21:34:15 +0530

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,7 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "Hello, uncle\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
+int main() {
+    cout << "Hello, Jenkins! Webhook triggered successfully!  PES2UG22CS169" << endl;
+    return 0;
 }
